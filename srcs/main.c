a06f49f1921a0ab3a116ca62d5217f1ef23b86d3
PageBlanche <axeldubois@orange.fr>
2025-04-14 10:38:10 +0200

    nothing

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-int main()
+int main(int ac , char **av)
 {
-	printf("Hello, World!\n");
+	if (ac < 2)
+		return (printf("ft_ping: usage error: Adresse de destination requise\n"));
+
+	char *ip = av[1];
+	
+	printf("ip = %s\n", ip);
+	return 0;
 }
